ada3c8928ea673bf39fccdf52545889a7f8b7bb9
Autumn Forsythe <autumn.forsythe@agilebits.com>
2026-05-02 17:08:31 -0400

    Users & homepage controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  skip_before_action :require_authentication, only: [:new, :create]
+
+  def new
+    redirect_to root_url if authenticated?
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    User.create user_params
+    redirect_to new_session_path
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.permit(user: [:email_address, :password])[:user]
+  end
+end
