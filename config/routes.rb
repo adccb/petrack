eb39192af9362e1f5e5625c5e9015b67cda94cc8
Autumn Forsythe <autumn.forsythe@agilebits.com>
2026-05-02 21:58:28 -0400

    Sidebar stuff

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   resources :users
+  resources :sidebar, only: :index
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
